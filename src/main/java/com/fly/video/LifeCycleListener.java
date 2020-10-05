dc95d1c3120e788e28b1f696e3cd9c7d4e17d26f
gx304419380 <304419380@qq.com>
2020-10-05 12:20:15 +0800

    本工具可以将主机做成文件下载服务器，用于局域网文件共享； 需要配合nginx一起工作

--- src/main/java/com/fly/video/LifeCycleListener.java
+++ src/main/java/com/fly/video/LifeCycleListener.java
@@ -2,11 +2,39 @@ package com.fly.video;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 @Component
 @Slf4j
 public class LifeCycleListener implements DisposableBean {
+
+    @Value("${nginx.path}")
+    private String nginxPath;
+
+    @EventListener(classes = ApplicationReadyEvent.class)
+    public void startNginx() throws IOException {
+        log.info("开始启动nginx...");
+        Runtime runtime = Runtime.getRuntime();
+        runtime.exec("cmd /k cd /d " + nginxPath + " && start nginx");
+
+        log.info("启动nginx完成");
+
+        // 获取IP地址
+        List<String> networkAddress = getNetworkAddress();
+        //打印
+        log.info("ip为：{}", networkAddress);
+    }
 
 
     @Override
@@ -14,10 +42,31 @@ public class LifeCycleListener implements DisposableBean {
 
         log.info("开始停止nginx...");
         Runtime runtime = Runtime.getRuntime();
-        runtime.exec("cmd /k cd /d D:\\nginx\\nginx-1.19.3 && " +
-                "nginx.exe -s stop");
+        runtime.exec("cmd /k cd /d "
+                + nginxPath
+                + " && nginx.exe -s stop");
         log.info("停止nginx完成");
     }
 
+
+    private List<String> getNetworkAddress() throws SocketException {
+        List<String> result = new ArrayList<>();
+        Enumeration<NetworkInterface> netInterfaces;
+
+        netInterfaces = NetworkInterface.getNetworkInterfaces();
+        InetAddress ip;
+        while (netInterfaces.hasMoreElements()) {
+            NetworkInterface ni = netInterfaces.nextElement();
+            Enumeration<InetAddress> addresses=ni.getInetAddresses();
+            while(addresses.hasMoreElements()){
+                ip = addresses.nextElement();
+                if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(':') == -1) {
+                    result.add(ip.getHostAddress());
+                }
+            }
+        }
+        return result;
+
+    }
 
 }
--- src/main/java/com/fly/video/VideoPlayerApplication.java
+++ src/main/java/com/fly/video/VideoPlayerApplication.java
@@ -4,50 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-
 @SpringBootApplication
 @Slf4j
 public class VideoPlayerApplication {
 
-    public static void main(String[] args) throws IOException {
-
-        log.info("开始启动nginx...");
-        Runtime runtime = Runtime.getRuntime();
-        runtime.exec("cmd /k cd /d D:\\nginx\\nginx-1.19.3 && start nginx");
-
-        // 获取IP地址
-        List<String> networkAddress = getNetworkAddress();
-        //打印
-        log.info("ip为：{}", networkAddress);
-
+    public static void main(String[] args) {
         SpringApplication.run(VideoPlayerApplication.class, args);
-    }
-
-    public static List<String> getNetworkAddress() throws SocketException {
-        List<String> result = new ArrayList<>();
-        Enumeration<NetworkInterface> netInterfaces;
-
-        netInterfaces = NetworkInterface.getNetworkInterfaces();
-        InetAddress ip;
-        while (netInterfaces.hasMoreElements()) {
-            NetworkInterface ni = netInterfaces.nextElement();
-            Enumeration<InetAddress> addresses=ni.getInetAddresses();
-            while(addresses.hasMoreElements()){
-                ip = addresses.nextElement();
-                if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(':') == -1) {
-                    result.add(ip.getHostAddress());
-                }
-            }
-        }
-        return result;
-
     }
 
 }
